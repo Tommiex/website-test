971656296a0b14801cd5eb6901404baa3d8119cf
Tommiex <103929877+Tommiex@users.noreply.github.com>
2022-07-01 01:52:16 +0700

    update nav

---
+++ website/src/compRestaurant/navbar/navbar.js
@@ -0,0 +1,12 @@
+import "./navbar.css";
+
+function Navbar(){
+    
+    return(
+        <div className="nav">
+            <div className="Left"></div>
+            <div className="Right"></div>
+        </div>
+    )
+}
+export default Navbar;
