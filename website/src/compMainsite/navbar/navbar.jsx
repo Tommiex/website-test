6b3c2acddacfa38b8db146650e198b3abcc78448
Tommiex <103929877+Tommiex@users.noreply.github.com>
2022-07-03 16:29:57 +0700

    firebase beginner

--- website/src/compMainsite/navbar/navbar.jsx
+++ website/src/compMainsite/navbar/navbar.jsx
@@ -3,36 +3,38 @@ import "./nav.css";
 const NavbarM1 = () => {
   return (
     <>
-      <div className="navBar">
-        <div id="nav-container">
-          <div className="bg"></div>
-          <div className="button" tabindex="0">
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </div>
-          <div id="nav-content" tabindex="0">
-            <ul>
-              <li>
-                <a href="#0">Home</a>
-              </li>
-              <li>
-                <a href="#0">Services</a>
-              </li>
-              <li>
-                <a href="#0">Blog</a>
-              </li>
-              <li>
-                <a href="#0">About</a>
-              </li>
-              <li>
-                <a href="#0">Contact</a>
-              </li>
-              <li className="small">
-                <a href="#0">Facebook</a>
-                <a href="#0">Instagram</a>
-              </li>
-            </ul>
+      <div>
+        <div className="navBar">
+          <div id="nav-container">
+            <div className="bg"></div>
+            <div className="button" tabindex="0">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </div>
+            <div id="nav-content" tabindex="0">
+              <ul>
+                <li>
+                  <a href="#0">Home</a>
+                </li>
+                <li>
+                  <a href="#0">Services</a>
+                </li>
+                <li>
+                  <a href="#0">Blog</a>
+                </li>
+                <li>
+                  <a href="#0">About</a>
+                </li>
+                <li>
+                  <a href="#0">Contact</a>
+                </li>
+                <li className="small">
+                  <a href="#0">Facebook</a>
+                  <a href="#0">Instagram</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
