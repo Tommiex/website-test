5df30e4f272f20ec5a776cd484f46fb921345041
kritin1124 <kritin@kkumail.com>
2022-07-03 11:52:02 +0700

    edit nav

--- website/src/compMainsite/navbar/navbar.jsx
+++ website/src/compMainsite/navbar/navbar.jsx
@@ -1,64 +1,43 @@
 import "./nav.css";
 
 const NavbarM1 = () => {
-    function onClick(){
-        document.getElementById('sidebarMenu')
-    }
+  function onClick() {
+    document.getElementById("sidebarMenu");
+  }
   return (
     <>
       <div>
-        <div className="header"></div>
-        <button type="checkbox" id="openSidebarMenu" className="openSidebarMenu" onClick={onClick()}></button>
-        
-        <label htmlFor="openSidebarMenu" className="sidebarIconToggle">
-          <div className="spinner diagonal part-1"></div>
-          <div className="spinner horizontal"></div>
-          <div className="spinner diagonal part-2"></div>
-        </label>
-        <div id="sidebarMenu">
-          <ul className="sidebarMenuInner">
-            <li>
-              Jelena Jovanovic <span>Web Developer</span>
-            </li>
-            <li>
-              <a href="https://vanila.io" target="_blank">
-                Company
-              </a>
-            </li>
-            <li>
-              <a href="https://instagram.com/plavookac" target="_blank">
-                Instagram
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/plavookac" target="_blank">
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.youtube.com/channel/UCDfZM0IK6RBgud8HYGFXAJg"
-                target="_blank"
-              >
-                YouTube
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/plavookac/" target="_blank">
-                Linkedin
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="center" className="main center">
-          <div className="mainInner">
-            <div>PURE CSS SIDEBAR TOGGLE MENU</div>
-          </div>
-          <div className="mainInner">
-            <div>PURE CSS SIDEBAR TOGGLE MENU</div>
-          </div>
-          <div className="mainInner">
-            <div>PURE CSS SIDEBAR TOGGLE MENU</div>
+        <div class="navBar">
+          <div id="nav-container">
+            <div class="bg"></div>
+            <div class="button" tabindex="0">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </div>
+            <div id="nav-content" tabindex="0">
+              <ul>
+                <li>
+                  <a href="#0">Home</a>
+                </li>
+                <li>
+                  <a href="#0">Services</a>
+                </li>
+                <li>
+                  <a href="#0">Blog</a>
+                </li>
+                <li>
+                  <a href="#0">About</a>
+                </li>
+                <li>
+                  <a href="#0">Contact</a>
+                </li>
+                <li class="small">
+                  <a href="#0">Facebook</a>
+                  <a href="#0">Instagram</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- website/src/compMainsite/siteMain.jsx
+++ website/src/compMainsite/siteMain.jsx
@@ -1,9 +1,13 @@
-import NavbarM1 from "./navbar/navbar"
-const SiteMain=() =>{
-    return(
-        <div>
-            <NavbarM1></NavbarM1>
-        </div>
-    )
-}
-export default SiteMain
+import NavbarM1 from "./navbar/navbar";
+import Slider from "./slider/slider";
+const SiteMain = () => {
+  return (
+    <div className="grid">
+      <div className="container">
+        <NavbarM1></NavbarM1>
+        
+      </div>
+    </div>
+  );
+};
+export default SiteMain;
