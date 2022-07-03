7b7c86614f3c5b0cd7b7169d298e95854df76262
Tommiex <103929877+Tommiex@users.noreply.github.com>
2022-07-04 05:32:09 +0700

    update rest hamburger

--- website/src/compRestaurant/navbar/navbar.js
+++ website/src/compRestaurant/navbar/navbar.js
@@ -1,12 +1,39 @@
 import "./navbar.css";
+import "./hamburger.css";
+import {useState}from "react"
 function Navbar1R(){
+    const [hamburgerBTN,changeHamburger]=useState()
+    
+    function activeBurger(){
+        changeHamburger(hamburgerBTN=> !hamburgerBTN)
+    }
+    function changeBurgerClass(){
+        if(hamburgerBTN){
+            return('hamburger HBGactive')
+        }else
+            return('hamburger')
+        
+    }
+    function changeRightClass(){
+        if(hamburgerBTN){
+            return('Right Ractive')
+        }else
+            return('Right')
+        
+    }
+
     
     return(
         <div className="nav">
             <div className="Left">
                 All
             </div>
-            <div className="Right">
+            <div className={changeBurgerClass()} onClick={()=>{activeBurger()}}>
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
+            </div>
+            <div className={changeRightClass()}>
                 <a href="#">Promotion</a>
                 <a href="#">Contact</a>
                 <a href="#">Back to main</a>
