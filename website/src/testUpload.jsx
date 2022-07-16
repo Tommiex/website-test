824867cde4ecb099defa700b1bba77ff0d9b1bdd
kritin1124 <kritin@kkumail.com>
2022-07-16 10:14:28 +0700

    edit radiobtn

--- website/src/testUpload.jsx
+++ website/src/testUpload.jsx
@@ -8,23 +8,28 @@ import {
 } from "firebase/storage";
 import { storage } from "./firebase";
 import { v4 } from "uuid";
+import { async } from "@firebase/util";
 
 const TestUpload = () => {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageUrls, setImageUrls] = useState([]);
   const [pathName, setPathname] = useState(null);
-  const uploadFile = async () => {
+  const imagesListRef = ref(storage, `${pathName}/`);
+  const UploadFile = async () => {
+    const cataogorie = useState(null);
+
     if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    const imageRef = ref(storage, `${pathName}/${imageUpload.name + v4()}`);
+
     const snapshot = await uploadBytes(imageRef, imageUpload);
     const url = await getDownloadURL(snapshot.ref);
-    const cataogorie = useState(null);
+
     setImageUrls((prev) => [...prev, url]);
     setPathname((prev) => [...prev, cataogorie]);
   };
 
   const fetchData = useCallback(async () => {
-    const imagesListRef = ref(storage, `${pathName}/`);
+    // const imagesListRef = ref(storage, `${pathName}/`);
     const response = await listAll(imagesListRef);
     const urls = [];
     for await (const item of response.items) {
@@ -42,6 +47,23 @@ const TestUpload = () => {
 
   useEffect(() => {
     fetchData();
+    const btn = document.querySelector("#btn");
+    const radioButtons = document.querySelectorAll('input[name="size"]');
+    const output = document.querySelector("#output");
+    btn.addEventListener("click", () => {
+      let selectedSize;
+      for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+          selectedSize = radioButton.value;
+          console.log(selectedSize);
+          break;
+        }
+      }
+      // show the output:
+      output.innerText = selectedSize
+        ? `You selected ${selectedSize}`
+        : `You haven't selected any size`;
+    });
   }, [fetchData]);
 
   return (
@@ -52,21 +74,26 @@ const TestUpload = () => {
           setImageUpload(event.target.files[0]);
         }}
       />
-      <button onClick={uploadFile}> Upload Image</button>
+      <button onClick={UploadFile}> Upload Image</button>
       {imageUrls.map((url, i) => {
         //return  รูปทั้งหมดมาใช้
         return <img src={url} key={i} alt="" />;
       })}
-      <div>
-        <input type="radio" id="age1" name="age" value="30"></input>
-        <label for="age1">0 - 30</label>
-        <br></br>
-        <input type="radio" id="age2" name="age" value="60"></input>
-        <label for="age2">31 - 60</label>
-        <br></br>
-        <input type="radio" id="age3" name="age" value="100"></input>
-        <label for="age3">61 - 100</label>
-        <br></br>
+      <div className="radiobtn">
+        <div>
+          <input type="radio" name="size" value="XS" id="xs"></input>
+          <label htmlFor="xs">XS</label>
+        </div>
+        <div>
+          <input type="radio" name="size" value="S" id="s"></input>
+          <label htmlFor="s">S</label>
+        </div>
+        <div>
+          <input type="radio" name="size" value="M" id="m"></input>
+          <label htmlFor="m">M</label>
+        </div>
+        <button id="btn">Show Selected Value</button>
+        <p id="output"></p>
       </div>
     </div>
   );
