e267eb3e7961ab3d0fda9a5694c2068bcb428ee3
Tommiex <103929877+Tommiex@users.noreply.github.com>
2022-07-19 14:54:42 +0700

    finish signup

--- website/src/auth/EmailSignUp.js
+++ website/src/auth/EmailSignUp.js
@@ -5,71 +5,47 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
-import React from "react";
+import React, { useEffect } from "react";
 import "./EmailSignUp.css";
 import { auth } from "../firebase";
 import { useState } from "react";
 
-export const txtEmail = document.querySelector("#txtEmail");
-export const txtPassword = document.querySelector("#txtPassword");
-export const txtPasswordConfirm = document.querySelector("#txtPasswordConfirm");
-
-export const btnLogin = document.querySelector("#btnLogin");
-export const btnSignup = document.querySelector("#btnSignup");
-export const btnLogout = document.querySelector("#btnLogout");
-
-export const divLoginError = document.querySelector("#divLoginError");
-export const lblLoginErrorMessage = document.querySelector(
-  "#lblLoginErrorMessage"
-);
-
-export const hideLoginError = () => {
-  divLoginError.style.display = "none";
-  lblLoginErrorMessage.innerHTML = "";
-};
-export const showLoginError = (error) => {
-  divLoginError.style.display = "block";
-  if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
-    lblLoginErrorMessage.innerHTML = `Wrong password. Try Again`;
-  } else {
-    lblLoginErrorMessage.innerHTML = `Error: ${error.message}`;
-  }
-};
-
-const ReSignup = ({onSubmit}) => {
-    const [pathName, setPathname] = useState(null);
-  const onFinish = (values) => {
-    console.log("Success:", values);
-    onSubmit(values);
-  };
-
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-  const signup = () => {
-    const handleSubmit = async (values) => {
-      const { email, password } = values;
-
-      try {
-        await createUserWithEmailAndPassword(auth, email, password);
-      } catch (error) {
-        alert(error);
+function ReSignup(){
+  var auth = getAuth();
+  
+  function signUp(){
+    const email = document.querySelector("#txtEmail").value
+    const password= document.querySelector("#txtPassword").value
+    
+    createUserWithEmailAndPassword(auth, email, password)
+    
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      const loginAlert= document.querySelector('.login-now')
+      if (user){
+        loginAlert.innerHTML="Logged in"
+      }else{
+        loginAlert.innerHTML="not logged in"
       }
-    };
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+    console.log(email)
+
+  }
+  
+    
 
     return (
-      <div
-      id="input-login"
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-
-        
-      >
-        <div className="login"
-        >
+      <div>
+        <div className="not-logged-in">
           <label>
             <span className="define">Email</span>
-            <input type="email" className="box-auth" id="txtEmail"></input>
+            <input type="email" className="box-auth" id="txtEmail" ></input>
           </label>
           <label>
             <span className="define">Password</span>
@@ -77,11 +53,13 @@ const ReSignup = ({onSubmit}) => {
               type="password"
               className="box-auth"
               id="txtPassword"
+            
             ></input>
           </label>
           <label>
             <span className="define">Password confirmation</span>
             <input
+             
               type="password"
               className="box-auth"
               id="txtPasswordConfirm"
@@ -92,20 +70,23 @@ const ReSignup = ({onSubmit}) => {
               Error message
             </div>
           </div>
+          <button type="button" id="btnLogin" >
+            Log in
+          </button>
+          <button type="button" id="btnSignup" onClick={signUp} >
+            Sign Up
+          </button>
         </div>
-        <button type="button" id="btnSignup" onClick={handleSubmit}>
-          Sign Up
-        </button>
-        <button type="button" id="btnLogin">
-          Log in
-        </button>
-        <button type="button" id="btnLogout">
-          Log out
-        </button>
+        <div id="logged-in">
+          <button type="button" id="btnLogout">
+            Log out
+          </button>
+        </div>
+        <div className="login-now"></div>
       </div>
     );
   };
 
   // btnLogin.addEventListener("click", loginEmailPassword)
-};
+
 export default ReSignup
